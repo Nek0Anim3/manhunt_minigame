6342a9e9e98c51871c278148b335e8d2031c67ce
Nek0Anim3 <pogoderopa@gmail.com>
2026-03-17 19:40:32 +0200

    Initial

---
+++ src/main/java/com/nekoanime/manhuntgame/Manhuntgame.java
@@ -0,0 +1,17 @@
+package com.nekoanime.manhuntgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Manhuntgame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
